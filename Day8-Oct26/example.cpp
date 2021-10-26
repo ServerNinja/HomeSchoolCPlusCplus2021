d95ee00a6cff397fb12ed5d38cbe99b708ce1c48
ServerNinja <serverninja@gmail.com>
2021-10-26 19:49:09 -0400

    Update example

--- Day8-Oct26/example.cpp
+++ Day8-Oct26/example.cpp
@@ -1,7 +1,7 @@
 /* 
   name: Random Number example 1
   author: Jennifer Reed
-  date: 9/23/2021
+  date: 10/26/2021
 
   description: Generate 10 random numbers between 1 and 100
 */
@@ -15,14 +15,14 @@ using namespace std;
 int main()
 {
     // Seed the randomizer - time(0) is the number of seconds since  Jan 1, 1970 00:00:00 UTC (also known as epoch time)
-	srand((int)time(0));
+    srand((int)time(0));
 
-	for(int i=0; i<=10; i++) {
+    for(int i=0; i<=10; i++) {
         // Generate a random number between 0 and 99 and add 1 to it
-		int r = (rand() % 100) + 1;
+        int r = (rand() % 100) + 1;
 
         // Output number
-		cout << r << " "<< endl;
+        cout << r << " "<< endl;
 	}
-	return(0);
+    return(0);
 }
