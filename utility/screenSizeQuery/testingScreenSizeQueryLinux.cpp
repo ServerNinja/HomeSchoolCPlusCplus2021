21995f1eeb4957f09a73825ebe2fca7dbb69976e
ServerNinja <serverninja@gmail.com>
2021-11-14 16:05:55 -0500

    Fixes to screenSizeDisplay utilities

--- utility/screenSizeQuery/testingScreenSizeQueryLinux.cpp
+++ utility/screenSizeQuery/testingScreenSizeQueryLinux.cpp
@@ -1,12 +1,24 @@
 #include <sys/ioctl.h>
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+struct termSize {
+    int array[2];
+};
+
+struct termSize terminalSize() {
+    struct winsize w;
+    ioctl(0, TIOCGWINSZ, &w);
+    return((struct termSize) { .array = {w.ws_row, w.ws_col} });
+}
 
 int main (void)
 {
-    struct winsize w;
-    ioctl(0, TIOCGWINSZ, &w);
+    struct termSize termSizeDimensions;
+    termSizeDimensions = terminalSize();
 
-    printf ("lines %d\n", w.ws_row);
-    printf ("columns %d\n", w.ws_col);
+    cout << "Rows:\t\t " << termSizeDimensions.array[0] << endl;
+    cout << "Columns:\t " << termSizeDimensions.array[1] << endl;
     return 0;
 }
--- utility/screenSizeQuery/testingScreenSizeQueryWindows.cpp
+++ utility/screenSizeQuery/testingScreenSizeQueryWindows.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <windows.h>
 
+struct termSize {
+    int array[2];
+};
+
+struct termSize terminalSize() {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns, rows;
+  
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    return((struct termSize) { .array = {rows, columns} });
+}
+
 int main (void)
 {
-  CONSOLE_SCREEN_BUFFER_INFO csbi;
-  int columns, rows;
+    struct termSize termSizeDimensions;
+    termSizeDimensions = terminalSize();
 
-  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-  columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-  rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-
-  printf("columns: %d\n", columns);
-  printf("rows: %d\n", rows);
-
-  return(0);
+    cout << "Rows:\t\t " << termSizeDimensions.array[0] << endl;
+    cout << "Columns:\t " << termSizeDimensions.array[1] << endl;
+    return(0);
 }
